c53947d6fce4782c51368ab21827118550239eec
João Victor Barros <joaovictor107543210@hotmail.com>
2022-12-13 21:14:20 -0300

    entidades

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import entities.Medico;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Medico obj = new Medico (1, "8080", "Davi");
+		System.out.println(obj);
+
+	}
+
+}
